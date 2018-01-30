e7026165e40d8a2b5aefe0360d74808a34d79d61
SravanthiPenti <sravanthi.penti@navaratan.com>
2018-01-30 16:50:24 +0530

    ng-click directive example

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,40 +4,17 @@
 
 var myapp = angular.module("angular1App", []);
 myapp.controller("myController", function ($scope) {
-    // var emp=[
-    // {firstname:"Ben",lastname:"Hasting",gender:"Male",salary:55000},
-    // {firstname:"Sara",lastname:"Paul",gender:"Female",salary:68000},
-    // {firstname:"Mark",lastname:"Holland",gender:"Male",salary:57000},
-    // {firstname:"Pam",lastname:"Macintosh",gender:"Female",salary:53500},
-    // {firstname:"Todd",lastname:"Barber",gender:"Male",salary:59500},
-    // ]
-    // $scope.emp=emp
-
-    var states = [
-
-        {
-            name: "Telangana",
-
-            dist: [
-                { name: "Karimnagar" },
-                { name: "Hyderabad" },
-                { name: "Warangal" }]
-        },
-        {
-            name: "Andhra Pradesh",
-
-            dist: [
-                { name: "West Godavari" },
-                { name: "Kurnool" },
-                { name: "East Godavari" }
-
-            ]
-
-
-        }
-
-
+    var technologies = [
+        {name:"Java",like:0,dislike:0},
+        {name:"JavaScript",like:0,dislike:0},
+        {name:"AngularJs",like:0,dislike:0},
+        {name:"Node",like:0,dislike:0}
     ]
-
-    $scope.states = states;
+    $scope.technologies=technologies;
+    $scope.incrementLike=function(technology){
+      technology.like++;
+    }
+    $scope.incrementDislike=function(technology){
+      technology.dislike++;
+    }
 })
