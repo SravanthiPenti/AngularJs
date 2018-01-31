d09af77cd98a2ee326c579a8d0c1868b3f50e4ea
SravanthiPenti <sravanthi.penti@navaratan.com>
2018-01-31 15:11:29 +0530

    Routing concepts in angularJs

---
+++ app/scripts/approute.js
@@ -0,0 +1,31 @@
+var app=angular.module("RouteDemo",["ngRoute"])
+                .config(function($routeProvider,$locationProvider){
+                    $routeProvider
+                            .when("/home",{
+                                templateUrl:"RouteTemplate/home.html",
+                                controller:("homeController")
+                            })
+                            .when("/table",{
+
+                                templateUrl:"RouteTemplate/table.html",
+                                controller:("tableController")
+                            })
+                            .when("/list",{
+                                templateUrl:"RouteTemplate/list.html",
+                                controller:("tableController")
+                            })
+                            $locationProvider.html5Mode(true);
+                })
+                .controller("homeController",function($scope){
+                    console.log("Inside home");
+                    $scope.msg="Welcome";
+                })
+                .controller("tableController",function($scope){
+                    var emp=[{firstname:"Ben",lastname:"Hasting",gender:1,salary:55000},
+                    {firstname:"Sara",lastname:"Paul",gender:2,salary:68000},
+                    {firstname:"Mark",lastname:"Holland",gender:1,salary:57000},
+                    {firstname:"Pam",lastname:"Macintosh",gender:2,salary:53500},
+                    {firstname:"Todd",lastname:"Barber",gender:1,salary:59500}]
+                    $scope.emp=emp;
+                })
+           
