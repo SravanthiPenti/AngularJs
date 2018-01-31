b19d71593aff31c6fd28eed245715ecb1e816ab6
SravanthiPenti <sravanthi.penti@navaratan.com>
2018-01-31 11:22:24 +0530

    ng-options example

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,3 +1,21 @@
-'use strict';
+var app=angular.module("AngularProject",[]);
+app.controller("myController",function($scope,$http){
+$http.get("views/httpservice.html").then(function(response){
+    $scope.message=response.data;
+})
+})
+app.controller("ServiceController",function($scope,$timeout){
+    $timeout(function(){
+        $scope.myTime="How are u?"},
+        2000);
+    })
+           
+        
 
-var emp=[{firstname:"Ben",lastname:"Hasting",gender:1,salary:55000},
+    app.controller("myNgOptionCtrl",function($scope){
+        $scope.cars=[
+            {model : "Ford Mustang", color : "red"},
+            {model : "Fiat 500", color : "white"},
+            {model : "Volvo XC90", color : "black"}
+        ]
+    })
